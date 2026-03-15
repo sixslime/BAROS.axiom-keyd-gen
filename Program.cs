55406cb69374d8a261c15fb707867ac1aa732d99
SixSlime <ryan.mm.taylor@gmail.com>
2026-03-15 14:07:08 -0700

    ok actual

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var input = Console.In.ReadToEnd();
-        var config = new ParsedConfig(input);
-
-
+        try
+        {
+            var input = Console.In.ReadToEnd();
+            var config = new ParsedConfig(input);
+            Console.Write(KeydGenerator.Generate(config));
+        }
+        catch (Exception e)
+        {
+            if (e is not (Tomlyn.TomlException or ProgramException)) throw;
+            Console.Error.Write(e.Message);
+        }
+        
     }
 }
